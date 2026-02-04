470e2e094628e57cc945ab827b341a0f446e0f7b
Simretemedhn <shimusimret@gmail.com>
2026-02-04 11:17:26 +0300

    Time: 17 ms (91.84%), Space: 19.4 MB (6.93%) - LeetHub

---
+++ 2652-sum-multiples/2652-sum-multiples.py
@@ -0,0 +1,9 @@
+class Solution:
+    def sumOfMultiples(self, n: int) -> int:
+        count = 0
+        for x in range(1, n+1):
+            if (x%3 == 0) or (x%5 == 0) or (x%7 ==0):
+                count += x
+        return count 
+
+        
