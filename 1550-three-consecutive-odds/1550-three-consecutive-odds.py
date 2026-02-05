86760adc0178f57a2fd1c60b152ca39b71c431ee
Simretemedhn <shimusimret@gmail.com>
2026-02-05 12:15:05 +0300

    Time: 0 ms (100%), Space: 19.4 MB (17.48%) - LeetHub

--- 1550-three-consecutive-odds/1550-three-consecutive-odds.py
+++ 1550-three-consecutive-odds/1550-three-consecutive-odds.py
@@ -1,8 +1,5 @@
 class Solution:
     def threeConsecutiveOdds(self, arr: List[int]) -> bool:
-        left = 0
-        right = 3
-        sliding_window =arr[left:right]
         for c in range(len(arr)-2):
             if arr[c] %2 != 0:
                 if arr[c+1]%2 != 0 and arr[c+2]%2 !=0:
