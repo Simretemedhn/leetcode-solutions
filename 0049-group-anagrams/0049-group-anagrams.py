86d6a3043b3a56b8a9014414a18fe7567ac9a6dd
Simretemedhn <shimusimret@gmail.com>
2026-02-06 16:56:24 +0300

    Time: 7 ms (98.09%), Space: 22.2 MB (44.03%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -1,16 +1,15 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        mp = {}
+        dicc = {}
+        for strss in strs:
+            anag = "".join(sorted(strss))
+       
+            if anag not in dicc:
+                dicc[anag] = []
+            dicc[anag].append(strss)
+        return list(dicc.values()) 
 
-        for word in strs:
-            key = "".join(sorted(word))
 
-            if key not in mp:
-                mp[key] = []
-
-            mp[key].append(word)
-
-        return list(mp.values())
 
             
 
