de67a4f9b0c032a9737ac450c9dedde1540bb292
Simretemedhn <shimusimret@gmail.com>
2026-02-12 11:02:12 +0300

    Time: 161 ms (28.16%), Space: 20.7 MB (13.45%) - LeetHub

---
+++ 1657-determine-if-two-strings-are-close/1657-determine-if-two-strings-are-close.py
@@ -0,0 +1,19 @@
+class Solution:
+    def closeStrings(self, word1: str, word2: str) -> bool:
+        char_map1 = {}
+        char_map2 = {}
+
+        for char1 in word1:
+            char_map1[char1] = char_map1.get(char1, 0) + 1
+        for char2 in word2:
+            char_map2[char2] = char_map2.get(char2, 0) + 1
+
+
+
+        char_map1_sum = sum(char_map1.values())
+        char_map2_sum = sum(char_map2.values())
+        if (sorted(char_map1) == sorted(char_map2)) and (sorted(char_map1.values()) == sorted(char_map2.values())):
+            return char_map1_sum == char_map2_sum
+        else:
+            return False 
+        
