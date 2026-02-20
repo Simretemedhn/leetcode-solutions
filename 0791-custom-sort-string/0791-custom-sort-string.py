b129ed3f8dd14d0b904e901c7a7fb8bc5582e93e
Simretemedhn <shimusimret@gmail.com>
2026-02-20 16:47:45 +0300

    Time: 0 ms (100%), Space: 19.6 MB (6.41%) - LeetHub

--- 0791-custom-sort-string/0791-custom-sort-string.py
+++ 0791-custom-sort-string/0791-custom-sort-string.py
@@ -1,20 +1,13 @@
-from collections import defaultdict 
+
+from collections import Counter
 class Solution:
     def customSortString(self, order: str, s: str) -> str:
-
-        order_map = defaultdict(lambda: -1)
-        for ind, char in enumerate(order):
-            order_map[char] = ind
-
-        list1 = list(s)
-        for i in range(len(s)-1):  
-            flag= False
-            for x in range(len(s)-1-i): 
-                if order_map[list1[x]] == -1:
-                    continue 
-                elif order_map[list1[x]] > order_map[list1[x+1]]:
-                    list1[x], list1[x+1] = list1[x+1], list1[x] 
-                    flag = True 
-            if not flag:
-                break
-        return "".join(list1)
+        s_count = Counter(s)
+        result = []
+        for x in order:
+            if x in s_count:
+                result.append(x * s_count[x])
+                s_count.pop(x)
+        for y in s_count:
+            result.append(y * s_count[y])
+        return "".join(result)
