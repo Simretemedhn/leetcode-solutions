31ce51751bcbca856b7ba35b92eed8896582f1cd
Simretemedhn <shimusimret@gmail.com>
2026-02-22 23:36:07 +0300

    Time: 3 ms (54.54%), Space: 19.4 MB (20.95%) - LeetHub

---
+++ 0389-find-the-difference/0389-find-the-difference.py
@@ -0,0 +1,8 @@
+class Solution:
+    def findTheDifference(self, s: str, t: str) -> str:
+        from collections import Counter
+        count_s = Counter(s)
+        count_t = Counter(t)
+        for ch in count_t:
+            if count_t[ch] != count_s.get(ch, 0):
+                return ch
