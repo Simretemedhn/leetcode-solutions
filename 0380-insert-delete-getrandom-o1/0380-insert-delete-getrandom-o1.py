2eec5fc5a5da17b404ac5303216b1c45f557a326
Simretemedhn <shimusimret@gmail.com>
2026-02-26 11:50:39 +0300

    Time: 97 ms (77.89%), Space: 57.2 MB (8.84%) - LeetHub

--- 0380-insert-delete-getrandom-o1/0380-insert-delete-getrandom-o1.py
+++ 0380-insert-delete-getrandom-o1/0380-insert-delete-getrandom-o1.py
@@ -1,32 +1,32 @@
 class RandomizedSet:
 
     def __init__(self):
-        self.val_to_index = {}
-        self.values = []
+        self.numMap = {}
+        self.numList = []
+        
 
     def insert(self, val: int) -> bool:
-        if val in self.val_to_index:
-            return False
-        self.val_to_index[val] = len(self.values)
-        self.values.append(val)
-        return True
+        res = val not in self.numMap
+        if res:
+            self.numMap[val] = len(self.numList)
+            self.numList.append(val)
+        return res
+        
 
     def remove(self, val: int) -> bool:
-        if val not in self.val_to_index:
-            return False
-        index = self.val_to_index[val]
-        last_val = self.values[-1]
-        
-        self.values[index] = last_val
-        self.val_to_index[last_val] = index
-        
-        self.values.pop()
-        del self.val_to_index[val]
-        return True
+        res = val in self.numMap
+        if res:
+            ind = self.numMap[val]
+            lastVal = self.numList[-1]
+            self.numList[ind] = lastVal
+            self.numList.pop()
+            self.numMap[lastVal] = ind
+            del self.numMap[val]
+        return res
 
     def getRandom(self) -> int:
-        import random
-        return random.choice(self.values)
+        return random.choice(self.numList)
+        
 
 
 # Your RandomizedSet object will be instantiated and called as such:
