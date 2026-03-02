a4f7d2dc23b8613980a3c261e7875f6b1813efbe
Simretemedhn <shimusimret@gmail.com>
2026-03-02 20:10:46 +0300

    Time: 0 ms (100%), Space: 19.3 MB (80.71%) - LeetHub

--- 1422-maximum-score-after-splitting-a-string/1422-maximum-score-after-splitting-a-string.py
+++ 1422-maximum-score-after-splitting-a-string/1422-maximum-score-after-splitting-a-string.py
@@ -1,16 +1,23 @@
-from collections import Counter 
 class Solution:
     def maxScore(self, s: str) -> int:
-        s_map = Counter(s)
-        l_level = 0
-        r_level = s_map["1"]
+        n = len(s)
+        
+        nums = [int(ch) for ch in s]
+        
+        prefix_ones = [0] * n
+        prefix_ones[0] = nums[0]
+        for i in range(1, n):
+            prefix_ones[i] = prefix_ones[i-1] + nums[i]
+        
+        total_ones = prefix_ones[-1]
         max_score = 0
-
-        for i in range(len(s)-1):
-            if s[i] == "0":
-                l_level += 1
-            else:
-                r_level -= 1
-            max_score = max(max_score, l_level + r_level)
-        return max_score 
+        
+        for i in range(n-1): 
+            zeros_in_left = (i + 1) - prefix_ones[i]
+            ones_in_right = total_ones - prefix_ones[i]
+            
+            score = zeros_in_left + ones_in_right
+            max_score = max(max_score, score)
+        
+        return max_score
 
