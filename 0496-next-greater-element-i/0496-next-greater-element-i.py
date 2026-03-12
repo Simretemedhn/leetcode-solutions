13bfeb440b6fd6b45eef6b71db369e8be0016436
Simretemedhn <shimusimret@gmail.com>
2026-03-12 16:41:20 +0300

    Time: 3 ms (40.27%), Space: 19.4 MB (62.76%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.py
+++ 0496-next-greater-element-i/0496-next-greater-element-i.py
@@ -2,6 +2,18 @@ class Solution:
     def nextGreaterElement(self, nums1: List[int], nums2: List[int]) -> List[int]:
         # stack
         # stay in the stack until a bigger element is found
+        num_map = {n:i for i, n in enumerate(nums1)}
+        res = [-1] * len(nums1)
+
+        for i in range(len(nums2)):
+            if nums2[i] not in num_map:
+                continue     
+            for j in range(i+1, len(nums2)):
+                if nums2[j] > nums2[i]:
+                    ind = num_map[nums2[i]]
+                    res[ind] = nums2[j] 
+                    break 
+        return res 
 
         stack = []
         greater = defaultdict(lambda : -1)
