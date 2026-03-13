9298bfc6bc7e9e45e58c0b89751430ab2a282c8c
Simretemedhn <shimusimret@gmail.com>
2026-03-13 10:03:31 +0300

    Time: 115 ms (64.06%), Space: 33.8 MB (40.37%) - LeetHub

--- 0304-range-sum-query-2d-immutable/0304-range-sum-query-2d-immutable.py
+++ 0304-range-sum-query-2d-immutable/0304-range-sum-query-2d-immutable.py
@@ -1,19 +1,24 @@
 class NumMatrix:
 
     def __init__(self, matrix: List[List[int]]):
-        rows = len(matrix)
-        cols = len(matrix[0])
-        self.prefix = [[0] * (cols + 1) for _ in range(rows + 1)]
-
-        for i in range(rows):
-            for j in range(cols):
-                self.prefix[i + 1][j + 1] = (matrix[i][j] +
-                                             self.prefix[i][j + 1] +
-                                             self.prefix[i + 1][j] -
-                                             self.prefix[i][j])
-
+        row = len(matrix)
+        col = len(matrix[0])
+        self.prefix = [[0] * (col + 1) for _ in range(row +1)]
+        for i in range(1, row+1):
+            for j in range(1, col+1):
+                self.prefix[i][j] = matrix[i-1][j-1] + self.prefix[i-1][j] + self.prefix[i][j-1] - self.prefix[i-1][j-1]
+        print(self.prefix)
+    
     def sumRegion(self, row1: int, col1: int, row2: int, col2: int) -> int:
-        return (self.prefix[row2 + 1][col2 + 1] -
-                self.prefix[row1][col2 + 1] -
-                self.prefix[row2 + 1][col1] +
-                self.prefix[row1][col1])
+        my_row1 = row1 + 1
+        my_col1 = col1 + 1
+        my_row2 = row2 + 1 
+        my_col2 = col2 + 1 
+        return self.prefix[my_row2][my_col2] - self.prefix[my_row1-1][my_col2] - self.prefix[my_row2][my_col1-1] + self.prefix[my_row1-1][my_col1-1]
+
+        
+
+
+# Your NumMatrix object will be instantiated and called as such:
+# obj = NumMatrix(matrix)
+# param_1 = obj.sumRegion(row1,col1,row2,col2)
