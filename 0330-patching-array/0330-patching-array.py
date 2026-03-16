31228243ffa4b74790a020c00896c87362b1759c
Simretemedhn <shimusimret@gmail.com>
2026-03-16 11:40:30 +0300

    Time: 3 ms (37.83%), Space: 19.3 MB (94.4%) - LeetHub

---
+++ 0330-patching-array/0330-patching-array.py
@@ -0,0 +1,43 @@
+class Solution:
+    def minPatches(self, nums: List[int], n: int) -> int:
+        total =  0
+        i = 0
+        cover = 0
+        total = 0
+        patch = 0
+        while total < n:
+            if total > cover:
+                cover = total
+            if i < len(nums) and cover+1 == nums[i]:
+                cover += 1
+                total += nums[i]
+                i += 1 
+            
+            elif i < len(nums) and cover+1 > nums[i]:
+                total += nums[i]
+                i += 1
+
+            else:
+                patch += 1
+                cover += 1
+                total += cover
+        return patch 
+
+#or simply 
+"""
+class Solution:
+    def minPatches(self, nums: List[int], n: int) -> int:
+        patches = 0
+        miss = 1  
+        i = 0
+        
+        while miss <= n:
+            if i < len(nums) and nums[i] <= miss:
+                miss += nums[i]
+                i += 1
+            else:
+                patches += 1
+                miss += miss 
+        
+        return patches
+"""
