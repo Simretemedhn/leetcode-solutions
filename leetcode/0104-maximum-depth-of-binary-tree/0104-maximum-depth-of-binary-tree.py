f2e712962dade9146374d824c9aa842fa658138f
Simretemedhn <shimusimret@gmail.com>
2026-03-19 14:06:32 +0300

    Add solution for Maximum Depth of Binary Tree

--- leetcode/0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.py
+++ leetcode/0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.py
@@ -4,31 +4,38 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
+class Solution:
+    def maxDepth(self, root: Optional[TreeNode]) -> int:
+        if not root:
+            return 0
+        q = deque([root])
+        level = 0
 
+        while q:
+
+            for i in range(len(q)):
+                node = q.popleft()
+                if node.left:
+                    q.append(node.left)
+                if node.right:
+                    q.append(node.right)
+            level += 1
+        return level 
 # or 
 """
-class Solution:
-    def maxDepth(self, root: Optional[TreeNode]) -> int:
+
         if not root:
             return 0
-        return 1 + max(self.maxDepth(root.left), self.maxDepth(root.right))
-""" 
-
-class Solution:
-    def maxDepth(self, root: Optional[TreeNode]) -> int:
-        if not root:
-            return 0
-
-        stack = [(root, 1)]
-        max_depth = 1
+        
+        stack = [[root, 1]]
+        res = 1
 
         while stack:
             node, depth = stack.pop()
-            max_depth = max(max_depth, depth)
-            
-            if node.left:
-                stack.append((node.left, depth + 1))
-            if node.right:
-                stack.append((node.right, depth + 1))
-        
-        return max_depth
+
+            if node:
+                res = max(res, depth)
+                stack.append([node.left, depth+1])
+                stack.append([node.right, depth + 1])
+        return res 
+"""
