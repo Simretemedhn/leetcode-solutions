9c6364ddf9368e0e5541a9e30a0724445fa404c7
Simretemedhn <shimusimret@gmail.com>
2026-03-19 11:31:38 +0300

    Add solution for Binary Tree Preorder Traversal

--- leetcode/0144-binary-tree-preorder-traversal/0144-binary-tree-preorder-traversal.py
+++ leetcode/0144-binary-tree-preorder-traversal/0144-binary-tree-preorder-traversal.py
@@ -5,11 +5,25 @@
 #         self.left = left
 #         self.right = right
 
-
 class Solution:
     def preorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
-        order = []
-
+        if not root:
+            return []
+        
+        result = []  
+        stack = [root]
+        
+        while stack:
+            node = stack.pop()
+            result.append(node.val)
+            
+            if node.right:
+                stack.append(node.right)
+            if node.left:
+                stack.append(node.left)
+        
+        return result
+"""
         def dfs(node):
             if not node:
                 return 
@@ -20,5 +34,5 @@ class Solution:
 
         dfs(root)  
         return order
-
+"""
 
