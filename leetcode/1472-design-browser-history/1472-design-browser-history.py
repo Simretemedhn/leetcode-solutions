3f1eff71aa0e4df1426de976ed8636c26cdd0a2b
Simretemedhn <shimusimret@gmail.com>
2026-03-23 13:06:42 +0300

    Add solution for Design Browser History

---
+++ leetcode/1472-design-browser-history/1472-design-browser-history.py
@@ -0,0 +1,25 @@
+class BrowserHistory:
+
+    def __init__(self, homepage: str):
+        self.back_stack = [homepage]   
+        self.forward_stack = []       
+
+    def visit(self, url: str) -> None:
+        self.back_stack.append(url)
+        self.forward_stack.clear()
+
+    def back(self, steps: int) -> str:
+        for _ in range(min(steps, len(self.back_stack) - 1)):
+            self.forward_stack.append(self.back_stack.pop())
+        return self.back_stack[-1]
+
+    def forward(self, steps: int) -> str:
+        for _ in range(min(steps, len(self.forward_stack))):
+            self.back_stack.append(self.forward_stack.pop())
+        return self.back_stack[-1]
+
+# Your BrowserHistory object will be instantiated and called as such:
+# obj = BrowserHistory(homepage)
+# obj.visit(url)
+# param_2 = obj.back(steps)
+# param_3 = obj.forward(steps)
