b7f03db351bdf49a59d766a3f0727937d7bd69b0
Simretemedhn <shimusimret@gmail.com>
2026-03-28 20:02:55 +0300

    Add solution for Subsets

--- leetcode/0078-subsets/0078-subsets.py
+++ leetcode/0078-subsets/0078-subsets.py
@@ -1,18 +1,19 @@
 class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
+
         res, subset = [], []
-
-        def dfs(i):
-            if i >= len(nums):
-                res.append(subset[:])
+        def backtrack(i):
+            if i == len(nums):
+                res.append(subset[::])
                 return 
-            
-            subset.append(nums[i])
-            dfs(i+1)
 
+            subset.append(nums[i])
+            backtrack(i+1)
             subset.pop()
-            dfs(i+1)
-        dfs(0)
+        
+            backtrack(i+1)
+        backtrack(0)
+
         return res 
-    
+            
         
