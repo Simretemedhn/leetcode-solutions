b5fddc49d236d039c2da5f335175f1bd762066c4
Simretemedhn <shimusimret@gmail.com>
2026-03-29 14:20:38 +0300

    Time: 45 ms (44.27%), Space: 33.5 MB (90.4%) - LeetHub

--- 0131-palindrome-partitioning/0131-palindrome-partitioning.py
+++ 0131-palindrome-partitioning/0131-palindrome-partitioning.py
@@ -1,23 +1,23 @@
 class Solution:
+    def ispal(self, s, i, j):
+        while i <= j:
+            if s[i] != s[j]:
+                return False 
+            i += 1
+            j -= 1 
+        return True 
     def partition(self, s: str) -> List[List[str]]:
-        result = []
-        current = []
-        
+        res, curr = [], []
+
         def backtrack(start):
             if start == len(s):
-                result.append(current[:])  
-                return
+                res.append(curr[:])
+                return 
             
-            for end in range(start + 1, len(s) + 1):
-                substring = s[start:end]
-
-                if substring == substring[::-1]:
-                    current.append(substring)
-                    backtrack(end)
-                    current.pop() 
-        
+            for j in range(start, len(s)):
+                if self.ispal(s, start, j):
+                    curr.append(s[start:j+1])
+                    backtrack(j+1)
+                    curr.pop()
         backtrack(0)
-        return result
-
-
-        
+        return res 
