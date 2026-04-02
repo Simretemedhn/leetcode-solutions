8255c15dcbccbe78a633c52ea1436172690d4c7d
Simretemedhn <shimusimret@gmail.com>
2026-04-02 11:18:31 +0300

    Add solution for Find First and Last Position of Element in Sorted Array

--- leetcode/0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.py
+++ leetcode/0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.py
@@ -1,39 +1,14 @@
+from bisect import bisect_left, bisect_right
+
 class Solution:
     def searchRange(self, nums: List[int], target: int) -> List[int]:
         if not nums:
             return [-1, -1]
-        def find_left(nums, target):
-            left = 0 
-            right = len(nums) - 1
-            ind = -1 
-            while left <= right:
-                mid = left + (right-left)//2
-                if nums[mid] == target:
-                    ind = mid 
-                    right = mid - 1 
-                elif nums[mid] > target:
-                    right  = mid - 1 
-                else:
-                    left = mid + 1 
-            return ind 
-        
-        def find_right(nums, target):
-            left = 0 
-            right  = len(nums) - 1 
-            ind = -1 
-
-            while left <= right:
-                mid  = left + (right - left) // 2 
-                if nums[mid] == target:
-                    ind = mid 
-                    left = mid + 1
-                elif nums[mid] > target:
-                    right = mid - 1 
-                else:
-                    left = mid + 1 
-            return ind 
-
-        first  = find_left(nums, target)
-        second = find_right(nums, target)
-        return [first, second]
-
+        i = bisect_left(nums, target)
+        if i < len(nums) and nums[i] == target:
+            start = i
+            j =  bisect_right(nums, target)
+            end = j -1 
+            return [start, end]
+        else:
+            return [-1, -1]
