4078206eb4758f5f0a62d65a72aef4d6843412cf
Simretemedhn <shimusimret@gmail.com>
2026-04-13 15:19:13 +0300

    Add solution for The k-th Lexicographical String of All Happy Strings of Length n

--- leetcode/1415-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n/1415-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n.py
+++ leetcode/1415-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n/1415-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n.py
@@ -1,27 +1,29 @@
 class Solution:
     def getHappyString(self, n: int, k: int) -> str:
-        if 3 * pow(2, n - 1) < k:
+        total = 3 * (1 << (n - 1)) 
+        if k > total:
             return ""
-        count = 0
-        letters = ["a", "b", "c"]
-        final = None 
         
-        def helper(subseq):
-            nonlocal count, final  
-            
-            if len(subseq) == n:  
-                count += 1 
-                if count == k:
-                    final = "".join(subseq)  
-                return 
-            
-            for letter in letters:
-                if not subseq or subseq[-1] != letter:
-                    subseq.append(letter)
-                    helper(subseq)
-                    subseq.pop() 
-                    if final: 
-                        return
+        result = []
+        k -= 1  
         
-        helper([])
-        return final if final else ""
+        group_size = total // 3
+        letters = ['a', 'b', 'c']
+        result.append(letters[k // group_size])
+        k %= group_size
+        
+        for i in range(1, n):
+            group_size //= 2
+            prev = result[-1]
+            if prev == 'a':
+                choices = ['b', 'c']
+            elif prev == 'b':
+                choices = ['a', 'c']
+            else: 
+                choices = ['a', 'b']
+        
+            result.append(choices[k // group_size])
+            k %= group_size
+        
+        return "".join(result)
+
