3ccd05528ef14b124f6c8589b26aae7e1ed16a32
Simretemedhn <shimusimret@gmail.com>
2026-04-13 09:28:09 +0300

    Add solution for Sqrt(x)

--- leetcode/0069-sqrtx/0069-sqrtx.py
+++ leetcode/0069-sqrtx/0069-sqrtx.py
@@ -1,18 +1,21 @@
 class Solution:
     def mySqrt(self, x: int) -> int:
+        if x == 0: return 0
+        if x < 4: return 1 
 
-        left = 1 
-        right = x 
+        low = 2
+        high = x//2
         res = 0
 
-        while left <= right:
-            mid = (left + right)//2 
-            if mid * mid > x:
-                right = mid - 1 
-            elif mid * mid < x:
+        while low <= high:
+            mid = (low + high)//2 
+            if mid * mid < x:
                 res = mid
-                left = mid + 1 
+                low = mid + 1 
+            elif mid * mid > x:
+                high = mid -1 
             else:
-                res = mid
-                break 
+                return mid 
         return res 
+        
+        
