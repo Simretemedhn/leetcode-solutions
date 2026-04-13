6e39e55c56aab968b0b587db30f09aee1a40fa20
Simretemedhn <shimusimret@gmail.com>
2026-04-13 08:58:59 +0300

    Add solution for Binary Search

--- leetcode/0704-binary-search/0704-binary-search.py
+++ leetcode/0704-binary-search/0704-binary-search.py
@@ -1,15 +1,13 @@
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
-        low = 0 
-        high = len(nums)-1
-
-        while low <= high:
-            mid = (low + high)//2 
-            if nums[mid] < target:
-                low = mid + 1 
-            elif nums[mid] > target:
-                high = mid - 1 
+        def helper(left, right):
+            if left > right:
+                return -1 
+            mid = (left + right)//2 
+            if nums[mid] > target:
+                return helper(left, mid-1)
+            elif nums[mid] < target:
+                return helper(mid+1, right)
             else:
                 return mid 
-        return -1 
-        
+        return helper(0, len(nums)-1)
