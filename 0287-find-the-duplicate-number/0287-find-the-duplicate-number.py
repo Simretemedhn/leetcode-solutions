0edaee623e85e9b4465caa5c07f3f26c8b543730
Simretemedhn <shimusimret@gmail.com>
2026-04-16 10:59:37 +0300

    Time: 19 ms (91.06%), Space: 33.7 MB (19.17%) - LeetHub

--- 0287-find-the-duplicate-number/0287-find-the-duplicate-number.py
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.py
@@ -1,29 +1,17 @@
 class Solution:
-    def findDuplicate(self, nums: List[int]) -> int:
-        n = len(nums)
-        for i in range(n):
-            while nums[i] != i + 1:
-                correct_pos = nums[i] - 1
-                if nums[correct_pos] == nums[i]:
-                    break
-                nums[i], nums[correct_pos] = nums[correct_pos], nums[i]        
-        for i in range(n):
-            if nums[i] != i + 1:
-                return nums[i]
+    def findDuplicate(self, nums: list[int]) -> int:
+        slow = nums[0]
+        fast = nums[0]
         
-        return -1
-
-"""
-class Solution:
-    def findDuplicate(self, nums: List[int]) -> int:
+        while True:
+            slow = nums[slow]         
+            fast = nums[nums[fast]]  
+            if slow == fast:
+                break
         
-        n = len(nums)
-        for i in range(n):
-
-            while nums[i] != i + 1 and nums[nums[i]-1] != nums[i]:
-                nums[i], nums[nums[i]-1] = nums[nums[i]-1], nums[i] 
-                print(nums)
-        for i in range(n):
-            if nums[i] != i + 1:
-                return nums[i]
-"""
+        slow = nums[0]
+        while slow != fast:
+            slow = nums[slow]         
+            fast = nums[fast]          
+            
+        return slow
