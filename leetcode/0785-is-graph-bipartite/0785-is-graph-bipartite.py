a120957f94de58d4d5ef1d1fe58fdf03fe0d3eb6
Simretemedhn <shimusimret@gmail.com>
2026-04-23 15:54:48 +0300

    Add solution for Is Graph Bipartite?

--- leetcode/0785-is-graph-bipartite/0785-is-graph-bipartite.py
+++ leetcode/0785-is-graph-bipartite/0785-is-graph-bipartite.py
@@ -1,44 +1,20 @@
 class Solution:
     def isBipartite(self, graph: List[List[int]]) -> bool:
-        n = len(graph)
-        mark = [-1] * n
+        colored = [-1] * len(graph)
+        def dfs(node, color):
+            colored[node] = color 
 
-        for i in range(n):
-            if mark[i] == -1:
-                stack = [i]
-                mark[i] = 0
-                
-                while stack:
-                    node = stack.pop()
-                    
-                    for nei in graph[node]:
-                        if mark[nei] == -1:
-                            mark[nei] = 1 - mark[node]  
-                            stack.append(nei)
-                        elif mark[nei] == mark[node]:
-                            return False
-        return True
-
-
-"""class Solution:
-    def isBipartite(self, graph: List[List[int]]) -> bool:
-        n = len(graph)
-        mark = [-1] * n
-
-        for ind, des in enumerate(graph):
-            if mark[ind] == -1:
-                mark[ind] = 0  
-            
-            if mark[ind] == 0:
-                child_mark = 1 
-            else:
-                child_mark = 0
-                
-            for nei in des:
-                if mark[nei] == -1:
-                    mark[nei] = child_mark
-                elif mark[nei] != child_mark:
+            for nei in graph[node]:
+                if colored[nei] == -1:
+                    if not dfs(nei, 1- color):
+                        return False 
+                elif colored[nei] == color:
                     return False 
+        
+            return True 
 
-        return True """
-
+        for i in range(len(graph)):
+            if colored[i] == -1:
+                if not dfs(i, 0):
+                    return False 
+        return dfs(0, 0)
