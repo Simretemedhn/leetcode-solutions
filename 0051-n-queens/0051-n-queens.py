d4fd0deac514130396a1441006c344ac409a0974
Simretemedhn <shimusimret@gmail.com>
2026-04-27 07:38:12 +0300

    Time: 39 ms (12.41%), Space: 19.7 MB (35.35%) - LeetHub

--- 0051-n-queens/0051-n-queens.py
+++ 0051-n-queens/0051-n-queens.py
@@ -1,36 +1,42 @@
 class Solution:
     def solveNQueens(self, n: int) -> List[List[str]]:
-
-
-        result = []
-        board = [['.' for _ in range(n)] for _ in range(n)]
-        cols = set()
-        pos_diag = set()
-        neg_diag = set()
+        collection = []
+        visited = set()  
         
-        def backtrack(row):
-            if row == n:
-                result.append([''.join(r) for r in board])
+        def is_safe(row, col):
+            for j in range(col):
+                if (row, j) in visited:
+                    return False
+            
+            i, j = row - 1, col - 1
+            while i >= 0 and j >= 0:
+                if (i, j) in visited:
+                    return False
+                i -= 1
+                j -= 1
+            
+            i, j = row + 1, col - 1
+            while i < n and j >= 0:
+                if (i, j) in visited:
+                    return False
+                i += 1
+                j -= 1
+            
+            return True
+        
+        def backtrack(col):
+            if col == n:
+                board = [['.' for _ in range(n)] for _ in range(n)]
+                for r, c in visited:
+                    board[r][c] = 'Q'
+                collection.append([''.join(row) for row in board])
                 return
             
-            for col in range(n):
-                if col in cols or (row + col) in pos_diag or (row - col) in neg_diag:
-                    continue
-                
-                board[row][col] = 'Q'
-                cols.add(col)
-                pos_diag.add(row + col)
-                neg_diag.add(row - col)
-                
-                backtrack(row + 1)
-                
-                board[row][col] = '.'
-                cols.remove(col)
-                pos_diag.remove(row + col)
-                neg_diag.remove(row - col)
+            for row in range(n):
+                if is_safe(row, col):
+                    visited.add((row, col))
+                    backtrack(col + 1)
+                    visited.remove((row, col))
         
         backtrack(0)
-        return result
-
-
-
+        return collection
