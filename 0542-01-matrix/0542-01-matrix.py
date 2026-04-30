05262342bffdb747d4a087ac5a34abc63e6c5b6b
Simretemedhn <shimusimret@gmail.com>
2026-04-30 21:12:23 +0300

    Time: 148 ms (32.18%), Space: 23.3 MB (13.74%) - LeetHub

---
+++ 0542-01-matrix/0542-01-matrix.py
@@ -0,0 +1,35 @@
+class Solution:
+    def updateMatrix(self, mat: List[List[int]]) -> List[List[int]]:
+        row = len(mat)               
+        col = len(mat[0])
+        q = deque()  
+        answer = [[0]*col for _ in range(row)]
+        visited = set()
+        
+        for r in range(row):
+            for c in range(col):
+                if mat[r][c] == 0:
+                    q.append((r, c, 0))  
+                    visited.add((r, c))      
+        
+        direction = [(1, 0), (0, 1), (-1, 0), (0, -1)]
+
+
+        while q:
+            rows, cols, length = q.popleft()
+            
+            for dr, dc in direction:
+                new_r, new_c = rows + dr, cols + dc
+                
+                if 0 <= new_r < row and 0 <= new_c < col:
+                    if (new_r, new_c) not in visited:
+                        if mat[new_r][new_c] == 1:
+                            answer[new_r][new_c] = length + 1  
+                        visited.add((new_r, new_c))
+                        q.append((new_r, new_c, length + 1))
+        
+        return answer
+
+
+
+        
