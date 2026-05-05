c6a01b90aecbeeb51ae9256cb205116bdb1e5434
Simretemedhn <shimusimret@gmail.com>
2026-05-05 08:37:28 +0300

    Time: 0 ms (100%), Space: 19.3 MB (89.97%) - LeetHub

--- 1129-shortest-path-with-alternating-colors/1129-shortest-path-with-alternating-colors.py
+++ 1129-shortest-path-with-alternating-colors/1129-shortest-path-with-alternating-colors.py
@@ -1,39 +1,38 @@
-from collections import deque, defaultdict
-
+from collections import defaultdict, deque
 class Solution:
-    def shortestAlternatingPaths(self, n: int, redEdges: List[List[int]], blueEdges: List[List[int]]) -> List[int]:    
-
-        def parse(edges):
-            e = defaultdict(list)  
-            for src, dst in edges:
+    def shortestAlternatingPaths(self, n: int, redEdges: List[List[int]], blueEdges: List[List[int]]) -> List[int]:
+        def parse(edge):
+            e = defaultdict(list)
+            for src, dst in edge:
                 e[src].append(dst)
             return e 
-            
+        
         red = parse(redEdges)
         blue = parse(blueEdges)
 
-        answer = [-1 for _ in range(n)]
         q = deque()
-        q.append((0, 0, None))  # (node, length, prev_color)
-        visit = set()
-        visit.add((0, None))
+        q.append((0, 0, None))  
+        answer = [-1] * n 
+        visited = set()
+        visited.add((0, None))  
 
         while q:
-            node, length, state = q.popleft() 
-
+            node, length, last_color = q.popleft()
+            
             if answer[node] == -1:
                 answer[node] = length 
-            
-            if state != "RED":
-                for nei in red[node]:
-                    if (nei, "RED") not in visit:
-                        visit.add((nei, "RED"))
-                        q.append((nei, length + 1, "RED"))
 
-            if state != "BLUE":
+            if last_color != "red":
+                for nei in red[node]:
+                    if (nei, "red") not in visited:
+                        visited.add((nei, "red"))
+                        q.append((nei, length + 1, "red"))
+
+            if last_color != "blue":
                 for nei in blue[node]:
-                    if (nei, "BLUE") not in visit:
-                        visit.add((nei, "BLUE"))
-                        q.append((nei, length + 1, "BLUE"))
+                    if (nei, "blue") not in visited:
+                        visited.add((nei, "blue"))
+                        q.append((nei, length + 1, "blue"))
+        
         return answer
 
