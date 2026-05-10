8d8420c78822080807737a60d9eb68d1315f33c0
Simretemedhn <shimusimret@gmail.com>
2026-05-10 13:15:44 +0300

    Time: 39 ms (64.62%), Space: 28.4 MB (28.49%) - LeetHub

--- 0802-find-eventual-safe-states/0802-find-eventual-safe-states.py
+++ 0802-find-eventual-safe-states/0802-find-eventual-safe-states.py
@@ -1,25 +1,64 @@
-from collections import defaultdict, deque 
-
 class Solution:
     def eventualSafeNodes(self, graph: List[List[int]]) -> List[int]:
-        rev_graph = defaultdict(list)
-        indegree  = [0] * len(graph)
-        
-        for i, dsts in enumerate(graph):
-            for every in dsts:
-                rev_graph[every].append(i)
-                indegree[i] += 1 
-        
-        q = deque([node for node in range(len(graph)) if indegree[node] == 0])
+        n = len(graph)
+        is_safe = {}
+
+        def dfs(i):
+            if i in is_safe:
+                return is_safe[i]
+            is_safe[i] = False 
+            for nei in graph[i]:
+                if not dfs(nei):
+                    return False 
+            is_safe[i] = True 
+            return True 
 
         answer = []
-        while q:
-            node = q.popleft()
-            answer.append(node)
-
-            for nei in rev_graph[node]:
-                indegree[nei] -= 1 
-                if indegree[nei] == 0:
-                    q.append(nei)
-        answer.sort()
+        for i in range(n):
+            if dfs(i):
+                answer.append(i)
         return answer 
+
+
+
+
+
+""" my first dump solution 
+        is_safe = {}
+
+        stack2 = []
+        def dfs(node):
+            stack = [node]
+            visited = set([node])  
+            last = None 
+            
+            while stack:
+                curr = stack.pop()
+                stack2.append(curr)
+                last = curr
+                
+                for nei in graph[curr]:
+                    if nei in visited:
+                        is_safe[nei] = False 
+                        return False 
+                    elif nei not in visited:
+                        visited.add(nei)  
+                        stack.append(nei)
+            is_safe[last] = True 
+            return True 
+        for i in range(len(graph)):
+            if i not in is_safe:
+                dfs(i)
+                last = stack2.pop()
+                if is_safe[last]:
+                    while stack2:
+                        node = stack2.pop()
+                        if node not in is_safe:
+                            dfs(node)
+                else:
+                    while stak2:
+                        node = stack2.pop()
+                        is_safe[node] = False 
+
+        return [i for i in range(len(is_safe)) if is_safe]  
+"""
