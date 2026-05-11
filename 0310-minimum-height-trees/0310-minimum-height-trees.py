9babbed576e58c92a179b514845eda7a100084a9
Simretemedhn <shimusimret@gmail.com>
2026-05-11 07:30:49 +0300

    Time: 50 ms (59.72%), Space: 27.8 MB (82.08%) - LeetHub

--- 0310-minimum-height-trees/0310-minimum-height-trees.py
+++ 0310-minimum-height-trees/0310-minimum-height-trees.py
@@ -1,50 +1,28 @@
-from collections import defaultdict, deque
-
+from collections import defaultdict, deque 
 class Solution:
     def findMinHeightTrees(self, n: int, edges: List[List[int]]) -> List[int]:
         if n == 1:
             return [0]
-            
         graph = defaultdict(list)
+        degree = [0] * n 
+
         for src, dst in edges:
             graph[src].append(dst)
             graph[dst].append(src)
+            degree[src] += 1
+            degree[dst] += 1 
         
-        q = deque([0])
-        visited = {0}
-        last = 0
-        
-        while q:
-            node = q.popleft()
-            last = node
-            for nei in graph[node]:
-                if nei not in visited:
-                    visited.add(nei)
-                    q.append(nei)
-        
-        parent = {}  
-        q = deque([last])
-        visited = {last}
-        parent[last] = -1  
-        farthest = last
-        
-        while q:
-            node = q.popleft()
-            farthest = node
-            for nei in graph[node]:
-                if nei not in visited:
-                    visited.add(nei)
-                    parent[nei] = node
-                    q.append(nei)
-        
-        path = []
-        current = farthest
-        while current != -1:
-            path.append(current)
-            current = parent[current]
+        q = deque([i for i in range(n) if degree[i] == 1])
+        remaining =  n 
+        while remaining > 2:
+            len_count = len(q)
+            remaining -= len_count 
+            for _ in range(len_count):
+                leaf = q.popleft()
 
-        path_length = len(path)
-        if path_length % 2 == 1:
-            return [path[path_length // 2]]
-        else:
-            return [path[path_length // 2 - 1], path[path_length // 2]]
+                for nei in graph[leaf]:
+                    degree[nei] -= 1 
+                    if degree[nei] == 1:
+                        q.append(nei)
+        return list(q)
+        
