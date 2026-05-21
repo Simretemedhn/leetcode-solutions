3b56f06cb5afd50ae204d5678d16ed06fbbc6f93
Simretemedhn <shimusimret@gmail.com>
2026-05-21 13:45:47 +0300

    Time: 0 ms (100%), Space: 19.4 MB (71.05%) - LeetHub

--- 1046-last-stone-weight/1046-last-stone-weight.py
+++ 1046-last-stone-weight/1046-last-stone-weight.py
@@ -1,55 +1,16 @@
+from heapq import heappush, heappop, heapify
 class Solution:
     def lastStoneWeight(self, stones: List[int]) -> int:
-        def swap(i, j):
-            stones[i], stones[j] = stones[j], stones[i]
-            
-        def heap_down(ind, size):  
-            left_child = 2 * ind + 1 
-            right_child = 2 * ind + 2 
-            largest_ind = ind 
+        max_heap = [-x for x in stones]
+        heapify(max_heap)
 
-            if left_child < size and stones[left_child] > stones[largest_ind]:
-                largest_ind = left_child 
-            if right_child < size and stones[right_child] > stones[largest_ind]:
-                largest_ind = right_child 
-            if largest_ind != ind:
-                swap(ind, largest_ind)
-                heap_down(largest_ind, size)
+        while len(max_heap) > 1:
+            first_larger = -heappop(max_heap)
+            second_larger = -heappop(max_heap)
+
+            diff = first_larger - second_larger 
+            if diff > 0:
+                heappush(max_heap, -diff)
         
-        def heapify(size):
-            for ind in range(size//2 - 1, -1, -1):
-                heap_down(ind, size)
-        
-        def remove(size):
-            removing = stones[0]
-            stones[0] = stones[size - 1]  
-            stones.pop()
-            heap_down(0, size - 1)  
-            return removing
-
-        def parent(child):
-            return (child - 1) // 2
-
-        def heap_up(current):
-            if current == 0:
-                return 
-            p = parent(current)
-            if stones[p] < stones[current]:
-                swap(p, current)
-                heap_up(p)
-
-        def add(num):
-            stones.append(num)
-            heap_up(len(stones) - 1)  
-
-        heapify(len(stones))
-        
-        while len(stones) > 1:  
-            size = len(stones)
-            removed_1 = remove(size)
-            removed_2 = remove(size - 1)  
-            
-            if removed_1 != removed_2:
-                add(abs(removed_1 - removed_2))
-        
-        return stones[0] if stones else 0
+        return -max_heap[0] if len(max_heap) == 1 else 0 
+         
