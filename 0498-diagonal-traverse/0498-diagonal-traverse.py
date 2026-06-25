922bbe4b78a3ca1ec6e708c0aaac8595da3191a6
Simretemedhn <shimusimret@gmail.com>
2026-06-25 13:32:26 +0300

    Time: 15 ms (40.36%), Space: 22.7 MB (9.54%) - LeetHub

--- 0498-diagonal-traverse/0498-diagonal-traverse.py
+++ 0498-diagonal-traverse/0498-diagonal-traverse.py
@@ -1,28 +1,16 @@
+from collections import defaultdict 
 class Solution:
     def findDiagonalOrder(self, mat: List[List[int]]) -> List[int]:
-        m = len(mat)
-        n = len(mat[0])
+        diagonal_sum_map_to_values  = defaultdict(list)
+
+        for row in range(len(mat)):
+            for col in range(len(mat[0])):
+                diagonal_sum_map_to_values[row + col].append(mat[row][col])
+        
         result = []
-        
-        for diagonal_sum in range(m + n - 1):
-            if diagonal_sum % 2 == 0: 
-                i = min(diagonal_sum, m - 1) 
-                j = diagonal_sum - i  
-                while i >= 0 and j < n:
-                    result.append(mat[i][j])
-                    i -= 1
-                    j += 1
-            else: 
-              
-                j = min(diagonal_sum, n - 1) 
-                i = diagonal_sum - j  
-                while j >= 0 and i < m:
-                    result.append(mat[i][j])
-                    i += 1
-                    j -= 1
-        
-        return result
+        for sum_ in diagonal_sum_map_to_values:
+            if sum_ % 2 == 0:
+                diagonal_sum_map_to_values[sum_].reverse()
+            result.extend(diagonal_sum_map_to_values[sum_])
+        return result 
 
-
-
-            
