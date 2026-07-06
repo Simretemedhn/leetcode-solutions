f53197a7e46aea8fc9708f110b5de4d83661c73c
Simretemedhn <shimusimret@gmail.com>
2026-07-06 10:10:54 +0300

    Time: 4 ms (1.26%), Space: 19.4 MB (18.69%) - LeetHub

--- 0027-remove-element/0027-remove-element.py
+++ 0027-remove-element/0027-remove-element.py
@@ -1,10 +1,37 @@
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
 class Solution:
     def removeElement(self, nums: List[int], val: int) -> int:
-        write  = 0 
-
-        for read in range(len(nums)):
-            if nums[read] != val:
-                nums[read], nums[write] = nums[write], nums[read]
-                write += 1 
+        # Convert array to linked list
+        dummy = ListNode(0)
+        current = dummy
         
-        return write 
+        # Build linked list from array (skip val)
+        for num in nums:
+            if num != val:
+                current.next = ListNode(num)
+                current = current.next
+        
+        # Count remaining nodes
+        count = 0
+        current = dummy.next
+        while current:
+            count += 1
+            current = current.next
+        
+        # Convert back to array (in-place)
+        i = 0
+        current = dummy.next
+        while current:
+            nums[i] = current.val
+            i += 1
+            current = current.next
+        
+        # Fill rest with placeholder (not required but good practice)
+        for i in range(count, len(nums)):
+            nums[i] = '_'  # Or any placeholder
+        
+        return count
