f675723ab5f6e9d30267a5a423109ca6fa985eb8
Simretemedhn <shimusimret@gmail.com>
2026-07-06 10:03:34 +0300

    Time: 0 ms (100%), Space: 19.3 MB (56.52%) - LeetHub

--- 0027-remove-element/0027-remove-element.py
+++ 0027-remove-element/0027-remove-element.py
@@ -1,9 +1,10 @@
 class Solution:
     def removeElement(self, nums: List[int], val: int) -> int:
-        write = 0 
+        write  = 0 
+
         for read in range(len(nums)):
             if nums[read] != val:
                 nums[read], nums[write] = nums[write], nums[read]
                 write += 1 
-
+        
         return write 
