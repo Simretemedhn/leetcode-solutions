6c2e5f4c48321507f73f166780c8902e537c1f08
Simretemedhn <shimusimret@gmail.com>
2026-07-06 14:55:44 +0300

    Time: 0 ms (100%), Space: 19.3 MB (23.61%) - LeetHub

--- 0202-happy-number/0202-happy-number.py
+++ 0202-happy-number/0202-happy-number.py
@@ -1,16 +1,21 @@
 class Solution:
     def isHappy(self, n: int) -> bool:
-        seen = set()
-        num = n
         
-        while num != 1:
-            if num in seen:
+        def digit_square(num):
+            sum_  = 0
+            while num > 0:
+                digit = num % 10 
+                sum_ += digit * digit
+
+                num //= 10 
+            return sum_ 
+        
+        slow = digit_square(n) 
+        fast = digit_square(digit_square(n))
+        while fast != 1:
+            slow = digit_square(slow)
+            fast = digit_square(digit_square(fast))
+
+            if slow == fast:
                 return False 
-            seen.add(num)
-
-            num_Str = str(num)
-            num = 0 
-            for every in num_Str:
-                num += int(every) ** 2 
-
         return True 
