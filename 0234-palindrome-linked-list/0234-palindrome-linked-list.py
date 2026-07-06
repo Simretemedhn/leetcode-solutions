0a2dfc2f2fcbd68b63e342492d4cf33c5c4b227b
Simretemedhn <shimusimret@gmail.com>
2026-07-06 15:34:28 +0300

    Time: 39 ms (40.75%), Space: 42.7 MB (57.2%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.py
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.py
@@ -5,17 +5,28 @@
 #         self.next = next
 class Solution:
     def isPalindrome(self, head: Optional[ListNode]) -> bool:
-        stack = []
         
-        curr = head
+        # to find the middle one 
+        slow, fast = head, head 
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next 
+        
+        # then reversing the second half 
+        prev = None 
+        curr = slow
         while curr:
-            stack.append(curr.val)
-            curr = curr.next 
-        origin = stack.copy()
-
-        reverse = []
-        while stack:
-            reverse.append(stack.pop())
-        return origin == reverse 
-
+            nex = curr.next
+            curr.next = prev 
+            prev = curr 
+            curr = nex
         
+        # checking equality of each node values 
+        curr = head
+        end = prev 
+        while curr and end:
+            if curr.val != end.val:
+                return False 
+            curr = curr.next
+            end = end.next
+        return True 
