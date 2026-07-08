697431ab8b12d84fa1c47ff0781495b5db6bc00e
Simretemedhn <shimusimret@gmail.com>
2026-07-08 12:14:08 +0300

    Time: 45 ms (91.52%), Space: 22.4 MB (43.03%) - LeetHub

--- 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.py
+++ 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.py
@@ -3,31 +3,27 @@
 #     def __init__(self, x):
 #         self.val = x
 #         self.next = None
-
 class Solution:
     def detectCycle(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        if not head:
-            return None
-            
-        slow = head
-        fast = head
-        has_cycle = False
         
+        # first intersection 
+        slow, fast = head, head
+        found = False
+
         while fast and fast.next:
-            slow = slow.next      
-            fast = fast.next.next   
-            
-            if slow == fast:        
-                has_cycle = True
-                break
+            slow = slow.next 
+            fast = fast.next.next 
+            if slow == fast:
+                found = True 
+                break 
         
-        if not has_cycle:
-            return None
-        
-        slow = head                
+        if not found:
+            return None  
+
+        slow = head  
         
         while slow != fast:
             slow = slow.next
-            fast = fast.next
+            fast = fast.next 
         
-        return slow  
+        return slow
