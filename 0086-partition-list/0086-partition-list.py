1607b91eac574632487fe902ae124bd86ef501f7
Simretemedhn <shimusimret@gmail.com>
2026-07-09 11:00:39 +0300

    Time: 0 ms (100%), Space: 19.2 MB (74.97%) - LeetHub

--- 0086-partition-list/0086-partition-list.py
+++ 0086-partition-list/0086-partition-list.py
@@ -3,60 +3,27 @@
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
-"""class Solution:
-    def partition(self, head: Optional[ListNode], x: int) -> Optional[ListNode]:
-        less_than = []
-        greater_than = []
-
-        curr = head 
-        while curr:
-            if curr.val < x:
-                less_than.append(curr.val)
-            elif curr.val > x:
-                greater_than.append(curr.val)
-        
-        dummy = ListNode(0)
-        last = dummy 
-        for i in range(len(less_than)):
-            new_node = ListNode(less_than[i])
-            last.next = new_node 
-            last = last.next 
-        
-        last.next = ListNode(x)
-        last = last.next 
-        for i in range(len(greater_than)):
-            new_node = ListNode(greater_than[i])
-            last.next = new_node 
-            last = last.next 
-        return dummy.next"""
-
 class Solution:
     def partition(self, head: Optional[ListNode], x: int) -> Optional[ListNode]:
-        if not head:
-            return head
+        # Create two dummy heads
+        less_dummy = ListNode(0)
+        greater_dummy = ListNode(0)
         
-        less_than = []
-        greater_than = []
-
-        curr = head 
+        less = less_dummy
+        greater = greater_dummy
+        
+        curr = head
         while curr:
             if curr.val < x:
-                less_than.append(curr.val)
-            else:  # FIX 2: >= x goes to greater_than
-                greater_than.append(curr.val)
-            curr = curr.next  # FIX 3: Move forward!
+                less.next = curr
+                less = less.next
+            else:
+                greater.next = curr
+                greater = greater.next
+            curr = curr.next
         
-        dummy = ListNode(0)
-        last = dummy 
+        # Connect the two lists
+        greater.next = None  # Terminate greater list
+        less.next = greater_dummy.next  # Connect less to greater
         
-        for val in less_than:
-            new_node = ListNode(val)
-            last.next = new_node 
-            last = last.next 
-        
-        for val in greater_than:
-            new_node = ListNode(val)
-            last.next = new_node 
-            last = last.next 
-        
-        return dummy.next
+        return less_dummy.next
