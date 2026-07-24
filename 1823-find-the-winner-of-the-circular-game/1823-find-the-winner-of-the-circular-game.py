de6a39ef257282a9c272da1fc98053e2b9a230c8
Simretemedhn <shimusimret@gmail.com>
2026-07-24 15:40:24 +0300

    Time: 47 ms (39.2%), Space: 19.2 MB (59.05%) - LeetHub

--- 1823-find-the-winner-of-the-circular-game/1823-find-the-winner-of-the-circular-game.py
+++ 1823-find-the-winner-of-the-circular-game/1823-find-the-winner-of-the-circular-game.py
@@ -1,10 +1,29 @@
+class ListNode:
+    def __init__(self, val):
+        self.val = val
+        self.next = None
+
 class Solution:
     def findTheWinner(self, n: int, k: int) -> int:
-        friends = list(range(1,n+1))
-        ind = 0 
+        
+        first = ListNode(1)
+        prev = first
+        
 
-        while len(friends) > 1:
-            ind = (ind + k - 1 ) % len(friends)
-            friends.pop(ind)
+        for i in range(2, n+1):
+            prev.next = ListNode(i)
 
-        return friends[0] 
+            prev = prev.next 
+        prev.next = first
+
+
+        remaining  = n 
+        while remaining > 1:
+
+            for _ in range(k-1):
+                prev = prev.next 
+            prev.next = prev.next.next
+            remaining -= 1 
+        
+        return prev.val 
+
