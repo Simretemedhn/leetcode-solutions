167a43754ab21f674c81878b8d6a2bd5d47ed811
Simretemedhn <shimusimret@gmail.com>
2026-07-28 18:40:55 +0300

    Time: 0 ms (100%), Space: 19.2 MB (90.35%) - LeetHub

---
+++ 0779-k-th-symbol-in-grammar/0779-k-th-symbol-in-grammar.py
@@ -0,0 +1,11 @@
+from collections import deque 
+class Solution:
+    def kthGrammar(self, n: int, k: int) -> int:
+        if n == 1:
+            return 0
+        prev_length = 2 ** (n-2)
+
+        if k <= prev_length:
+            return self.kthGrammar(n-1, k)
+        else:
+            return 1 - self.kthGrammar(n-1, k - prev_length)
