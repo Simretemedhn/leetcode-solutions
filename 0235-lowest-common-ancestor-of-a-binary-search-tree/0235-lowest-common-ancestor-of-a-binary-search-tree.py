5e52d973a5b9675a2911b914b10282a698877d81
Simretemedhn <shimusimret@gmail.com>
2026-08-10 17:00:16 +0300

    Time: 60 ms (81.21%), Space: 22.8 MB (18.01%) - LeetHub

--- 0235-lowest-common-ancestor-of-a-binary-search-tree/0235-lowest-common-ancestor-of-a-binary-search-tree.py
+++ 0235-lowest-common-ancestor-of-a-binary-search-tree/0235-lowest-common-ancestor-of-a-binary-search-tree.py
@@ -7,16 +7,18 @@
 
 class Solution:
     def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
+        # if it is bigger that both we have to go to the left 
+        # if it is smaller than both we have to go to the right 
+        # if it is in the middle of both, it is the number 
+        # if one of the number if found it is the number 
         if not root:
             return None 
-        elif root == p or root == q:
+        if root == p or root == q:
+            return root  
+        elif p.val < root.val and q.val < root.val:
+            return self.lowestCommonAncestor(root.left, p, q)
+        elif p.val > root.val and q.val > root.val:
+            return self.lowestCommonAncestor(root.right, p, q) 
+        else:
             return root
-
-        left = self.lowestCommonAncestor(root.left, p ,q)
-        right = self.lowestCommonAncestor(root.right, p ,q)
-        if left and right:
-            return root
-
-        return left if left else right
-
 
